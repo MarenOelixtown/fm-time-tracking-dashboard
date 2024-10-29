3b2a42b3bee7e71a7c2ad5a5bbff8c4ef59ac2c8
Maren <marenehlers@gmx.net>
2024-10-29 18:29:17 +0100

    fetching data from json-file

---
+++ js/index.js
@@ -0,0 +1,14 @@
+fetch("/data.json")
+  .then((request) => {
+    if (!request.ok) {
+      console.log("Oops! Something went wrong.");
+      return null;
+    }
+
+    return request.json();
+  })
+  .then((data) => {
+    /* console.log(data); */
+    const activities = data;
+    console.log(activities);
+  });
