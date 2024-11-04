5080871e60b5598e5166f765af337ee66426ed3b
Maren <marenehlers@gmx.net>
2024-11-04 10:55:28 +0100

    changing data-variable-naming, connecting timeframe-buttons with timeframe-container

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const activityList = document.querySelector('[data-js="activity-list"]');
 
-let activities = null;
 fetch("/data.json")
   .then((request) => {
     if (!request.ok) {
@@ -11,32 +10,32 @@ fetch("/data.json")
     return request.json();
   })
   .then((data) => {
-    activities = data;
-    console.log(activities);
-    activities.forEach((item) => {
-      appendActivity(item);
+    const allActivities = data;
+    console.log(allActivities);
+    allActivities.forEach((activity) => {
+      appendActivity(activity);
     });
   });
 
-const appendActivity = (item) => {
+const appendActivity = (activity) => {
   const newActivity = document.createElement("li");
   newActivity.classList.add("activity-list__item");
   newActivity.innerHTML = `
     <button aria-pressed="false">
     <span class="sr-only">Menu</span>
     <img src="./images/icon-ellipsis.svg"/></button>    
-    <h3>${item.title}</h3>
-    <div class="timeframe-content--hidden visible" data-js="daily">
-      <p>${item.timeframes.daily.current}hours</p>
-      <p>Last week - ${item.timeframes.daily.previous}hours</p>
+    <h4>${activity.title}</h4>
+    <div id="timeframe-daily" aria-labelledby="daily" tabindex="0" class="timeframe-content--hidden">
+      <p>${activity.timeframes.daily.current}hours</p>
+      <p>Last week - ${activity.timeframes.daily.previous}hours</p>
     </div>
-    <div class="timeframe-content--hidden" data-js="weekly">
-      <p>${item.timeframes.weekly.current}hours</p>
-      <p>Last week - ${item.timeframes.weekly.previous}hours</p>
+    <div id="timeframe-weekly" aria-labelledby="weekly" tabindex="0" class="timeframe-content--hidden">
+      <p>${activity.timeframes.weekly.current}hours</p>
+      <p>Last week - ${activity.timeframes.weekly.previous}hours</p>
     </div>
-    <div class="timeframe-content--hidden" data-js="monthly">
-      <p>${item.timeframes.monthly.current}hours</p>
-      <p>Last week - ${item.timeframes.monthly.previous}hours</p>
+    <div id="timeframe-monthly" aria-labelledby="weekly" tabindex="0" class="timeframe-content--hidden">
+      <p>${activity.timeframes.monthly.current}hours</p>
+      <p>Last week - ${activity.timeframes.monthly.previous}hours</p>
     </div>`;
   activityList.append(newActivity);
 };
