e366b34ac3f39e5359b0be3d609586c10fa8adfb
Maren <marenehlers@gmx.net>
2024-11-04 16:25:41 +0100

    adding changeTimeframe-function injs-file forbuttons

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const activityList = document.querySelector('[data-js="activity-list"]');
-
 fetch("/data.json")
   .then((request) => {
     if (!request.ok) {
@@ -16,26 +15,59 @@ fetch("/data.json")
       appendActivity(activity);
     });
   });
-
 const appendActivity = (activity) => {
   const newActivity = document.createElement("li");
   newActivity.classList.add("activity-list__item");
+  newActivity.setAttribute("data-js", "activity-item");
   newActivity.innerHTML = `
-    <button aria-pressed="false">
-    <span class="sr-only">Menu</span>
-    <img src="./images/icon-ellipsis.svg"/></button>    
-    <h4>${activity.title}</h4>
-    <div id="timeframe-daily" aria-labelledby="daily" tabindex="0" class="timeframe-content--hidden">
-      <p>${activity.timeframes.daily.current}hours</p>
-      <p>Last week - ${activity.timeframes.daily.previous}hours</p>
-    </div>
-    <div id="timeframe-weekly" aria-labelledby="weekly" tabindex="0" class="timeframe-content--hidden">
-      <p>${activity.timeframes.weekly.current}hours</p>
-      <p>Last week - ${activity.timeframes.weekly.previous}hours</p>
-    </div>
-    <div id="timeframe-monthly" aria-labelledby="weekly" tabindex="0" class="timeframe-content--hidden">
-      <p>${activity.timeframes.monthly.current}hours</p>
-      <p>Last week - ${activity.timeframes.monthly.previous}hours</p>
-    </div>`;
+      <button aria-pressed="false">
+      <span class="sr-only">Menu</span>
+      <img src="./images/icon-ellipsis.svg"/></button>    
+      <h4>${activity.title}</h4>
+      <div id="timeframe-daily" role="tabpanel" aria-labelledby="daily" class="timeframe-content">
+        <p>${activity.timeframes.daily.current}hours</p>
+        <p>Yesterday - ${activity.timeframes.daily.previous}hours</p>
+      </div>
+      <div id="timeframe-weekly" role="tabpanel" aria-labelledby="weekly" class="timeframe-content" hidden>
+        <p>${activity.timeframes.weekly.current}hours</p>
+        <p>Last week - ${activity.timeframes.weekly.previous}hours</p>
+      </div>
+      <div id="timeframe-monthly" role="tabpanel" aria-labelledby="monthly" class="timeframe-content" hidden>
+        <p>${activity.timeframes.monthly.current}hours</p>
+        <p>Last month - ${activity.timeframes.monthly.previous}hours</p>
+      </div>`;
   activityList.append(newActivity);
 };
+
+const timeframeButtonList = document.querySelector(
+  '[data-js="btn-group-timeframe"]'
+);
+const allTimeframeButtons = timeframeButtonList.querySelectorAll(
+  ':scope > [role="tab"]'
+);
+allTimeframeButtons.forEach((button) => {
+  button.addEventListener("click", changeTimeframe);
+});
+
+function changeTimeframe(event) {
+  const targetTimeframeBtn = event.target;
+  const selectedTimeframe = targetTimeframeBtn.getAttribute("aria-controls");
+  const allActivityItems = document.querySelectorAll(
+    '[data-js="activity-item"]'
+  );
+
+  allTimeframeButtons.forEach((button) =>
+    button.setAttribute("aria-selected", false)
+  );
+
+  targetTimeframeBtn.setAttribute("aria-selected", true);
+
+  allActivityItems.forEach((activityItem) => {
+    activityItem
+      .querySelectorAll('[role="tabpanel"]')
+      .forEach((timeframe) => timeframe.setAttribute("hidden", ""));
+    activityItem
+      .querySelector(`#${selectedTimeframe}`)
+      .removeAttribute("hidden");
+  });
+}
